a0cd888de67c1b020c9b6415c7a8b2fad65cbfb2
Joe Ahern <jrahern@rdnt.co>
2016-11-30 21:07:07 -0600

    Icons

--- www/php/views/approach.php
+++ www/php/views/approach.php
@@ -34,7 +34,15 @@
                 <div class="text">Listen Up<span>.</span></div>
             </div>
             <div class="block infograph">
-                <img src="images/approach/graph-icon.png" width="160" alt="Image">
+                <div class="svg-icon">
+                    <svg version="1.1" id="Layer_8" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="200px" height="200px" viewBox="0 0 200 200" enable-background="new 0 0 200 200" xml:space="preserve">
+                        <path id="line-8-1" d="M6.7,122.7 30.8,122.7 45,142.9 57.1,122.7 74.6,122.7 82,111.9 88.1,122.7 102.9,122.7 116.4,91.6 131.2,122.7 149.6,122.7 183.7,66.3" stroke="#000" stroke-width="5" fill="none" style="stroke-dasharray: 0; stroke-dashoffset: 0;"></path>
+                        <g id="graph-lines">
+                            <path id="line-8-2" class="graph-line" d="M186.9,64l-28.6,8.5" stroke="#000" stroke-width="5" fill="none" style="stroke-dasharray: 0; stroke-dashoffset: 0;"></path>
+                            <path id="line-8-3" class="graph-line" d="M183.8,62.3l8.5,28.6" stroke="#000" stroke-width="5" fill="none" style="stroke-dasharray: 0; stroke-dashoffset: 0;"></path>
+                        </g>
+                    </svg>
+                </div>
                 <div class="summary">We analyze search and behavioral data to understand what people are seeking now, and in the future.</div>
             </div>
         </div>
